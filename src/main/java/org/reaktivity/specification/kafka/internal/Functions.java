c7df322e21dd405efd6b9a39a734ea3db30cd42c
John Fallows <john.fallows@kaazing.com>
2017-10-06 20:26:15 -0700

    Fetch scripts

---
+++ src/main/java/org/reaktivity/specification/kafka/internal/Functions.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2016-2017 The Reaktivity Project
+ *
+ * The Reaktivity Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package org.reaktivity.specification.kafka.internal;
+
+import java.util.Random;
+
+import org.kaazing.k3po.lang.el.Function;
+import org.kaazing.k3po.lang.el.spi.FunctionMapperSpi;
+
+public final class Functions
+{
+    private static final Random RANDOM = new Random();
+
+    @Function
+    public static int newRequestId()
+    {
+        return RANDOM.nextInt();
+    }
+
+    private Functions()
+    {
+        // utility
+    }
+
+    public static class Mapper extends FunctionMapperSpi.Reflective
+    {
+
+        public Mapper()
+        {
+            super(Functions.class);
+        }
+
+        @Override
+        public String getPrefixName()
+        {
+            return "kafka";
+        }
+    }
+}
---
+++ src/test/java/org/reaktivity/specification/kafka/internal/FunctionsTest.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2016-2017 The Reaktivity Project
+ *
+ * The Reaktivity Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package org.reaktivity.specification.kafka.internal;
+
+import static org.junit.Assert.assertTrue;
+import static org.kaazing.k3po.lang.internal.el.ExpressionFactoryUtils.newExpressionFactory;
+
+import javax.el.ELContext;
+import javax.el.ExpressionFactory;
+import javax.el.ValueExpression;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.kaazing.k3po.lang.internal.el.ExpressionContext;
+
+public class FunctionsTest
+{
+
+    private ExpressionFactory factory;
+    private ELContext ctx;
+
+    @Before
+    public void setUp() throws Exception
+    {
+
+        factory = newExpressionFactory();
+        ctx = new ExpressionContext();
+    }
+
+    @Test
+    public void shouldInvokeNewRequestId() throws Exception
+    {
+        String expressionText = "${kafka:newRequestId()}";
+        ValueExpression expression = factory.createValueExpression(ctx, expressionText, Integer.class);
+        Object value = expression.getValue(ctx);
+        assertTrue(value instanceof Integer);
+    }
+}
