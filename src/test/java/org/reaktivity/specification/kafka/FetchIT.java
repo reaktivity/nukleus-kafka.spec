7d262dcfe126f9dc58df11b8e1343fc2b7247416
Chris Barrow <chris.barrow@kaazing.com>
2018-08-20 13:06:52 -0700

    Fixed header.zero.offset.repeated scripts to avoid sporadic test failures

--- src/test/java/org/reaktivity/specification/kafka/FetchIT.java
+++ src/test/java/org/reaktivity/specification/kafka/FetchIT.java
@@ -737,6 +737,7 @@ public class FetchIT
     {
         k3po.start();
         k3po.notifyBarrier("ROUTED_SERVER");
+        k3po.notifyBarrier("WRITE_FETCH_RESPONSE");
         k3po.finish();
     }
 
